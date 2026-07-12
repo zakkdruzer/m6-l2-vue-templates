f26e06ff52d0d29d744f0f43448367704db4e1a2
zakkdruzer <zakkdruzer@gmail.com>
2026-07-12 17:38:04 -0300

    fix: se modifica el index.html para visualizacion en gitpages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/m6-l2-vue-templates/', // 👈 nombre exacto de tu repo
+  base: '/m6-l2-vue-templates/',
 })
